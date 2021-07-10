e126ca1ae589437f84b5410c4dd783e44c25f3f2
sravya <n.ragasravya@gmail.com>
2021-07-10 17:39:10 +0530

    RewardingFitness

---
+++ app/src/main/java/com/example/rewardingfitness/Fitnessgames.java
@@ -0,0 +1,27 @@
+package com.example.rewardingfitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Fitnessgames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitnessgames);
+    }
+
+    public void Pushups(View view) {
+    }
+
+    public void Balance(View view) {
+    }
+
+    public void Boxing(View view) {
+    }
+
+    public void Zumba(View view) {
+    }
+}
---
+++ app/src/main/java/com/example/rewardingfitness/splash.java
@@ -0,0 +1,32 @@
+package com.example.rewardingfitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import java.util.concurrent.RunnableFuture;
+
+public class splash extends AppCompatActivity {
+
+    TextView textView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TextView textView=findViewById(R.id.appname);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        }, 3500);
+
+    }
+}
